356d7060cfee9990c0dfcbfa297bc20de1563e8a
kgranch <ksg56@nau.edu>
2015-03-23 12:20:53 -0700

    Update Game.hx

--- src/Game.hx
+++ src/Game.hx
@@ -18,12 +18,22 @@ import starling.display.Stage;
 import flash.media.Sound;
 import flash.media.SoundChannel;
 import flash.media.SoundTransform;
+import flash.ui.GameInput;
+import flash.ui.GameInputControl;
+import flash.ui.GameInputDevice;
+import flash.events.GameInputEvent;
 import starling.text.TextField;
 import flash.utils.Dictionary;
 
 class GameDriver extends Sprite {
   // Global assets manager
   public static var assets:AssetManager;
+  
+  //Initialize it immediately - VERY important to now move this.
+  var gi = new GameInput();
+  
+  //score
+  var Score:Text;
   
   //buttons
   var MainMenu:Button;
@@ -33,6 +43,11 @@ class GameDriver extends Sprite {
   var Grass:Sprite;
   var GameOver:Sprite;
   var Mushroom:Sprite;
+  
+  gi.addEventListener("device_added", handleDeviceAdded);
+}
+
+function handleDeviceAdded(event:DeviceAdded) {
 }
 
 function onKeyDown(event:KeyboardEvent) {
@@ -45,4 +60,34 @@ function onKeyDown(event:KeyboardEvent) {
     if (event.keyCode == Keyboard.RIGHT){
       
     }
+}
+
+function scrollMap(){
+  screenMatrix = new Matrix(SCREEN_COLS, SCREEN_ROWS);
+ 
+  for (var rowCtr:int = 0;rowCtr < SCREEN_ROWS; rowCtr++)
+  {
+    for (var colCtr:int = 0; colCtr < SCREEN_COLS; colCtr++)
+    {
+	      //var tileSprite:Sprite = new Sprite();
+	      var image:Image = new Image(tilesetVector[levelMatrix.GetItem(colCtr, rowCtr)]);
+	      //tileSprite.addChild(image);
+        //tileSprite.flatten();					
+ 
+	    //screenMatrix.PutItem(colCtr, rowCtr, tileSprite);
+  	  screenMatrix.PutItem(colCtr, rowCtr, image);
+	    screenMatrix.GetItem(colCtr, rowCtr).x = colCtr * TILE_WIDTH;
+	    screenMatrix.GetItem(colCtr, rowCtr).y = rowCtr * TILE_HEIGHT;
+	    addChild(screenMatrix.GetItem(colCtr, rowCtr));
+ 
+      }
+    }
+  }
+  
+}
+
+//Insert the map reading here
+function readMap(){
+  int get_x;
+  int get_y;
 }
